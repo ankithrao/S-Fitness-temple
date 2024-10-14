6b221ad8b91a540507377b2f21184cacaa986c4a
ankithrao <ankithrao0005@gmail.com>
2024-10-14 18:54:01 +0530

    Add button and animated the text and button

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,7 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import Homepage from "./welcomeScreen/Homepage";
 
 export default function Index() {
-  return (
-    <View className='flex-1 flex justify-end'>
-      <StatusBar style='light'/>
-      <Image className='h-full w-full absolute' source={require('../assets/images/welcome.png')}/>
-    </View>
-  )
+  return <Homepage />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],//specify the folder where you want to add tailwind css
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
